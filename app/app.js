30d457db7e8c89dc2ebf2e1f6b38558237710fc1
Andrey Shkurpela <andrey@appstone.co.il>
2016-09-07 16:37:32 +0300

    first commit

--- app/app.js
+++ app/app.js
@@ -1,14 +1,51 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+var app = angular.module('myApp', [
+  'ui.router',
+ 
+]);
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+app.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', function($locationProvider, $stateProvider, $urlRouterProvider) {
+	
+
+	$stateProvider.state('/', {    
+        url: "/",
+        template: "<front></front>"
+    });
+
+	$stateProvider.state('login', {    
+        url: "/login",
+        template: "<login></login>"
+    });
+    
+$urlRouterProvider.otherwise('/');
+
+ 
 }]);
+
+app.component('front', {
+	templateUrl: "pages/front.html",
+	 controller: function(){
+        var ctrl = this;
+        console.log('in front now');}
+});
+
+app.component('login', {
+	templateUrl: "pages/login.html",
+	 controller: function($scope){
+        var ctrl = this;
+        console.log('in login now');
+
+        ctrl.login = function(){
+        	console.log(ctrl.email, ctrl.password, ctrl.remember);
+        	ctrl.re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        	console.log($scope.login.email.$valid);
+        	console.log($scope.login);
+        	// if (ctrl.re.test(ctrl.email)){alert('val')}
+        	// 	else{alert('noVal');}
+        };
+
+
+    }
+});
