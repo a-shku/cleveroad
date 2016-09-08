d09aab8d73d053b5aaa8aedcfb6dd6e554f9ba61
Andrey Shkurpela <andrey@appstone.co.il>
2016-09-08 14:51:18 +0300

    added modal window for user editing

--- app/app.js
+++ app/app.js
@@ -3,12 +3,14 @@
 // Declare app level module which depends on views, and components
 var app = angular.module('myApp', [
   'ui.router',
-  'ngMessages'
+  'ngMessages',
+  'ui.bootstrap',
+  'ngAnimate',
+
  
 ]);
 
 app.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', function($locationProvider, $stateProvider, $urlRouterProvider) {
-	
 
 	$stateProvider.state('/', {    
         url: "/",
@@ -19,34 +21,117 @@ app.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', functio
         url: "/login",
         template: "<login></login>"
     });
-    
-$urlRouterProvider.otherwise('/');
 
- 
+    $stateProvider.state('member', {    
+        url: "/member",
+        template: "<front member-view='true'></front>"
+    });
+    
+	$urlRouterProvider.otherwise('/');
 }]);
+
+app.component('nHeader', {
+	templateUrl: "pages/partials/header.html",
+	controller: function($rootScope, $uibModal, $log){
+		console.log('header is working');
+
+	var ctrl = this;
+
+		/*modal*/
+  //        ctrl.open = function (size) {
+	 //    	$uibModal.open({
+	 //      animation: true,
+	 //      ariaLabelledBy: 'modal-title',
+	 //      ariaDescribedBy: 'modal-body',
+	 //      templateUrl: 'pages/edit-user-modal.html',
+	 //      controller: 'ModalInstanceCtrl',
+	 //      controllerAs: '$ctrl',
+	 //      size: size,
+	 //      // resolve: {
+	 //      //   items: function () {
+	 //      //     return $ctrl.items;
+	 //      //   }
+	 //      // }
+	 //    });
+		// }
+	/*/modal*/
+	/*mod component*/
+	ctrl.openComponentModal = function () {
+    var modalInstance = $uibModal.open({
+      animation: ctrl.animationsEnabled,
+      component: 'modalComponent',
+      resolve: {
+        items: function () {
+          return ctrl.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      ctrl.selected = selectedItem;
+    }, function () {
+      $log.info('modal-component dismissed at: ' + new Date());
+    });
+  };
+  /*/mod component*/
+
+	}
+});
+
+app.component('nFooter', {
+	templateUrl: "pages/partials/footer.html",
+	controller: function($rootScope, $uibModal){
+		console.log('footer is working');
+	}
+});
 
 app.component('front', {
 	templateUrl: "pages/front.html",
-	 controller: function(){
+	bindings: {
+        //typeNavbar: "@",
+        memberView: "@",
+        //adminView: "@"
+    },
+	 controller: function($rootScope, $uibModal){
         var ctrl = this;
-        console.log('in front now');}
+        console.log('in front now');
+        console.log($rootScope.ActiveUser);
+        ctrl.ActiveUser = $rootScope.ActiveUser;
+
+        ctrl.func = function(){
+        	alert(111)
+        }
+
+        
+
+
+    }
 });
 
 app.component('login', {
 	templateUrl: "pages/login.html",
-	 controller: function($scope){
+	 controller: function($rootScope, $scope, $location){
         var ctrl = this;
         console.log('in login now');
 
         ctrl.login = function(){
         	console.log(ctrl.email, ctrl.password, ctrl.remember);
-        	ctrl.re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        	ctrl.emailReg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        	ctrl.passReg = /[a-zA-Z0-9]/
         	console.log($scope.login.email.$valid);
         	console.log($scope.login);
-        	// if (ctrl.re.test(ctrl.email)){alert('val')}
+        	// if (ctrl.emailReg.test(ctrl.email)){alert('val')}
         	// 	else{alert('noVal');}
-        	//if(){$location.url('/member');}
-        	 console.log('error', $scope.login.email.$error);
+        	//if(response from server is 'OK'){$location.url('/member');}
+        	console.log('all form', $scope.login.$valid);
+        	if($scope.login.$valid ){
+        		$rootScope.ActiveUser = {
+        			email: ctrl.email,
+        			password: ctrl.password
+        		}
+        		$location.url('/member')
+        	}
+        	 console.log('error', $scope.login.$error);
         	 console.log($scope.login.email.$valid);
         };
 
@@ -54,4 +139,35 @@ app.component('login', {
 
 
     }
+});
+
+app.controller('ModalInstanceCtrl', function($uibModalInstance){
+	console.log('modalWindow was opened');
+})
+
+app.component('modalComponent', {
+  templateUrl: 'pages/edit-user-modal.html',
+  bindings: {
+    resolve: '<',
+    close: '&',
+    dismiss: '&'
+  },
+  controller: function () {
+    var ctrl = this;
+
+    ctrl.$onInit = function () {
+      ctrl.items = ctrl.resolve.items;
+      ctrl.selected = {
+        item: ctrl.items[0]
+      };
+    };
+
+    ctrl.ok = function () {
+      ctrl.close({$value: ctrl.selected.item});
+    };
+
+    ctrl.cancel = function () {
+      ctrl.dismiss({$value: 'cancel'});
+    };
+  }
 });
